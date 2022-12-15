a787bd71bedc2cab478205d99f0982ab91f1f04f
Awnish Dubey <dubeyawnish6@gmail.com>
2022-12-15 11:31:04 +0530

    top loading bar

--- src/App.js
+++ src/App.js
@@ -2,53 +2,111 @@ import "./App.css";
 import React, { Component } from "react";
 import Navbar from "./Components/Navbar";
 import News from "./Components/News";
+import LoadingBar from "react-top-loading-bar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 export class App extends Component {
   static propTypes = {};
-
+  state = {
+    progress: 0,
+  };
+  setProgress = (progress) => {
+    this.setState({ progress: progress });
+  };
   render() {
     return (
       <div>
         <Router>
           <Navbar />
-
+          <LoadingBar
+            height={3}
+            color="#f11946"
+            progress={this.state.progress}
+          />
           <Routes>
-            <Route exact path="/" element={<News category="general" />}></Route>
+            <Route
+              exact
+              path="/"
+              element={
+                <News setProgress={this.setProgress} category="general" />
+              }
+            ></Route>
             <Route
               exact
               path="/business"
-              element={<News key="business" category="business" />}
+              element={
+                <News
+                  setProgress={this.setProgress}
+                  key="business"
+                  category="business"
+                />
+              }
             ></Route>
             <Route
               exact
               path="/entertainment"
-              element={<News key="entertainment" category="entertainment" />}
+              element={
+                <News
+                  setProgress={this.setProgress}
+                  key="entertainment"
+                  category="entertainment"
+                />
+              }
             ></Route>
             <Route
               exact
               path="/general"
-              element={<News key="general" category="general" />}
+              element={
+                <News
+                  setProgress={this.setProgress}
+                  key="general"
+                  category="general"
+                />
+              }
             ></Route>
             <Route
               exact
               path="/health"
-              element={<News key="health" category="health" />}
+              element={
+                <News
+                  setProgress={this.setProgress}
+                  key="health"
+                  category="health"
+                />
+              }
             ></Route>
             <Route
               exact
               path="/science"
-              element={<News key="science" category="science" />}
+              element={
+                <News
+                  setProgress={this.setProgress}
+                  key="science"
+                  category="science"
+                />
+              }
             ></Route>
             <Route
               exact
               path="/sports"
-              element={<News key="sports" category="sports" />}
+              element={
+                <News
+                  setProgress={this.setProgress}
+                  key="sports"
+                  category="sports"
+                />
+              }
             ></Route>
             <Route
               exact
               path="/technology"
-              element={<News key="technology" category="technology" />}
+              element={
+                <News
+                  setProgress={this.setProgress}
+                  key="technology"
+                  category="technology"
+                />
+              }
             ></Route>
           </Routes>
         </Router>
--- src/Components/News.js
+++ src/Components/News.js
@@ -27,15 +27,19 @@ export class News extends Component {
   }
 
   async updateNews() {
+    this.props.setProgress(0);
     const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=8bc9f831a62841888d1d976f567ae8cf&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     this.setState({ loading: true });
     let data = await fetch(url);
+    this.props.setProgress(30);
     let parseData = await data.json();
+    this.props.setProgress(70);
     this.setState({
       articles: parseData.articles,
       totalArticles: parseData.totalResults,
       loading: false,
     });
+    this.props.setProgress(100);
   }
 
   async componentDidMount() {
